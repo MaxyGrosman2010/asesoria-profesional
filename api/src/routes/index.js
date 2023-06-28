f201c35dafb74ffe504fb668afc78004dc81227d
MaxyGrosman <maxygrosman@gmail.com>
2023-06-28 14:07:01 -0300

    Ejemplo .env

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,25 +1,27 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // const getRouter = require('./getRouter.route');
-const createServiceRouter = require('./createService.route');
-const createTypeServiceRouter = require('./createTypeService.route');
-const getAllTypeServiceRouter = require('./getAllTypeService.route');
-const getAllServiceRouter = require('./getAllServices.route');
-const getServiceById = require('./getServiceById.route');
-const getServiceByName = require('./getServiceByName');
-const getLogInHandler = require('../handlers/getLogInGoogleHandler');
-require('../middleware/passport');
-
-
+const createServiceRouter = require("./createService.route");
+const createTypeServiceRouter = require("./createTypeService.route");
+const getAllTypeServiceRouter = require("./getAllTypeService.route");
+const getAllServiceRouter = require("./getAllServices.route");
+const getServiceById = require("./getServiceById.route");
+const getServiceByName = require("./getServiceByName");
+const getLogInHandler = require("../handlers/getLogInGoogleHandler");
+require("../middleware/passport");
+const singUpRouter = require("./singUp.router");
+const singInRouter = require("./singIn.router");
 
 const router = Router();
 
 // router.use(getRouter);
-router.use('/service', createServiceRouter);
-router.use('/typeService', createTypeServiceRouter);
-router.use('/allTypeService', getAllTypeServiceRouter);
-router.use('/allService', getAllServiceRouter);
-router.use('/serviceById', getServiceById);
-router.use('/nameService', getServiceByName);
+router.use("/service", createServiceRouter);
+router.use("/typeService", createTypeServiceRouter);
+router.use("/allTypeService", getAllTypeServiceRouter);
+router.use("/allService", getAllServiceRouter);
+router.use("/serviceById", getServiceById);
+router.use("/nameService", getServiceByName);
+router.use("/singUp", singUpRouter);
+router.use("/singIn", singInRouter);
 //!REFERIDO A LOGIN GOOGLE
 router.get("/loginGoogle", getLogInHandler.getLogin);
 router.get("/auth", getLogInHandler.authenticate);
