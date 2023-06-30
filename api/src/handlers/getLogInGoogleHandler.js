453c3cf28e9831c89a891e2d07cb147ace431dc8
Martin Garrido <martin.garrido@gmail.com>
2023-06-30 10:11:50 -0300

    foto para id

--- api/src/handlers/getLogInGoogleHandler.js
+++ api/src/handlers/getLogInGoogleHandler.js
@@ -15,12 +15,15 @@ const getLogInHandler = {
   }),
   loginSuccess: (req, res, next) => {
     if (!req.user) res.redirect('/auth/callback/failure');
+    console.log(req.user);
     //res.send('Welcome ' + req.user.email);
     const dataUser = req.user;
-    const { displayName, email } = req.user;
+    const { id, displayName, email, profilePict, photos } = req.user;
     const frontUser = {
+      id: id,
       name: displayName,
       email: email,
+      profilePict: photos[0],
     };
     //console.log(dataUser);
     res.send(`
