d383de59d3bf0fc58d7dcb3ad2fe691a5f41f219
FedeOrefici <federicoaorefici@gmail.com>
2023-07-07 15:56:13 -0300

    update

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ dotenv.config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_TABLE} = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://postgres:federicoorefici@localhost/profesionales`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_TABLE}`,
   {
     logging: false,
     native: false,
