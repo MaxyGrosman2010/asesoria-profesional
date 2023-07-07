9c5f112c36c92c1a99d812aeffa565ae9ed30229
MaxyGrosman <maxygrosman@gmail.com>
2023-07-07 15:51:09 -0300

    Update Service By Id

--- api/src/controllers/allUsers.controller.js
+++ api/src/controllers/allUsers.controller.js
@@ -1,6 +1,7 @@
 const {User, Service, TypeService} = require('../db');
 
 const getAllUsers = (id) => User.findAll({
+    
     include: [
         {
             model: Service,
--- api/src/handlers/getServiceById.handler.js
+++ api/src/handlers/getServiceById.handler.js
@@ -23,9 +23,9 @@ const getServiceById = async(req, res) => {
     });
 
     const {type} = TypeServices[0];
-    const result = {id, name, price, description, files, type, user_id};
+    const result = {id, name, price, description, files, type, user_id, review};
 
-    return res.status(200).json({result, review});
+    return res.status(200).json({result});
         
   }catch(error){
 
