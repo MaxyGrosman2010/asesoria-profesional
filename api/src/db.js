1b728007ce9c4b70aad14b312e48f356b213d584
MaxyGrosman <maxygrosman@gmail.com>
2023-07-12 14:58:20 -0300

    update

--- api/src/db.js
+++ api/src/db.js
@@ -8,13 +8,13 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_TABLE, DB_DEPLOY } =
 
 //↓↓↓COMENTAR PARA USA LA BASE DE DATOS LOCAL, ESTO ES LA DB DEPLOYADA EN RAILWAY
 
-const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false,
-  native: false,
-});
+// const sequelize = new Sequelize(DB_DEPLOY, {
+//   logging: false,
+//   native: false,
+// });
 
 //DESCOMENTAR PARA USAR LOCAL
-/*
+
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_TABLE}`,
   {
@@ -22,7 +22,7 @@ const sequelize = new Sequelize(
     native: false,
   }
 );
-*/
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
